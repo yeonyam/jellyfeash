33737c763f765d1439f454d0e67ffb264aa76548
yeonyam <noabyeon@gmail.com>
2026-02-26 00:30:53 -0500

    commit

--- sculpture.js
+++ sculpture.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const titleEl = document.getElementById("detail-title");
+  const textEl = document.getElementById("detail-text");
   const galleryEl = document.getElementById("detail-gallery");
 
   //click
   const DATA = {
     a: {
-      title: "back",
+      title: "you don't see me (2025)",
+      text: "steel, chiffon fabric, and wool",
       images: ["sculpture/IMG_5382.JPG", "sculpture/IMG_5430.jpg", "sculpture/IMG_5431.jpg"],
     },
 
     b: {
-      title: "face",
+      title: "an experiment",
+      text: "oil painted on beeswax with 3D printed spines and glycerine-gelatin bio plastics",
       images: ["sculpture/IMG_0549.jpg", "sculpture/IMG_0562.JPG", "sculpture/IMG_0580.jpg"],
     },
 
@@ -20,17 +23,19 @@ document.addEventListener("DOMContentLoaded", () => {
     },
 
     d: {
-      title: "org",
+      title: "불가살이",
+      text: "metal, dragonskin, wool and wax",
       images: ["sculpture/ex-121.JPG", "sculpture/ex-122.JPG", "sculpture/ex-128.jpg", "sculpture/ex-137.JPG"],
     },
 
     e: {
-      title: "pillow",
+      title: "known faces",
+      text: "metal, glass, silicone, vinyl tubing, and water",
       images: []
     },
 
     f: {
-      title: "washing",
+      title: "washing machine",
       images: []
     },
     };
@@ -41,21 +46,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const key = el.dataset.key;
       const item = DATA[key];
-      if(!item) return;
+
+      if(!item) {
+        console.error("data에 이 key가 없음", key);
+        return;
+      }
+
+      const list = item.items || item.images;
+      if (!Array.isArray(list)) {
+        console.error("items/images가 배열이 아님:", key, item);
+        return;
+      }
 
       //title
-      titleEl.textContent = item.title;
+      titleEl.textContent = item.title || "";
+      textEl.textContent = item.text || "";
 
       //empty gallery
       galleryEl.innerHTML = "";
 
       //fill image
-      item.images.forEach((src) => {
-        const img = document.createElement("img");
-        img.src = src;
-        img.alt = item.title;
-        galleryEl.appendChild(img);
-      });
+      
+      if (Array.isArray(item.images)) {
+        item.images.forEach((src) => {
+          const img = document.createElement("img");
+          img.src = src;
+          img.alt = item.title || "";
+          img.loading = "lazy";
+          galleryEl.appendChild(img);
+        });
+      } else {
+        console.error("images가 배열이 아님:", key, item);
+      }
 
       //하단 스크롤
       document.getElementById("detail").scrollIntoView({behavior:"smooth"});
